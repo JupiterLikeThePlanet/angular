52d0c60b2c886dabdae8d49f1b98e16f54189295
Jupiter <jupiterbaudot@gmail.com>
2019-11-21 16:22:25 -0800

    Add ServerComponent to Module and move server folder

--- app00/src/app/app.module.ts
+++ app00/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-
-import {FormsModule} from '@angular/forms'
-
+// import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+
+import { ServerComponent } from './server/server.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    // HttpModule,
     FormsModule
   ],
   providers: [],
