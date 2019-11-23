c002bc04856acf82cac5bf47d9857c7bdf08bf39
Jupiter <jupiterbaudot@gmail.com>
2019-11-22 16:27:29 -0800

    Render selectors three different ways

--- app00/src/app/server/server.component.ts
+++ app00/src/app/server/server.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
+    // selector can create an html element 'app-server', or
+    // selector: 'app-server',
+    //an attribute '[app-server]', or
+    // selector: '[app-server]',
+    // a class '.app-server'
+    selector: '.app-server',
     templateUrl:'./server.component.html',
     // styleUrls:['./server.component.css'],
     styles: [`
