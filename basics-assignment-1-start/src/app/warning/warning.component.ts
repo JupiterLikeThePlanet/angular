558c19651736a46e55842f39dcd9b36f06ed4cfe
Jupiter <jupiterbaudot@gmail.com>
2019-11-26 13:05:10 -0800

    Style warning component

--- basics-assignment-1-start/src/app/warning/warning.component.ts
+++ basics-assignment-1-start/src/app/warning/warning.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'warning-alert',
     templateUrl: './warning.component.html',
-    styles: [`
-        p {
-            color: red;
-        }
-    `]
+    styleUrls: ['./warning.component.css']
+    // styles: [`
+    //     p {
+    //         color: red;
+    //     }
+    // `]
 })
 
 export class WarningComponent {
