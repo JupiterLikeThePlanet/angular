7f4d5d5fe1eab6af320091e54b32bb3e4fcf1369
Jupiter <jupiterbaudot@gmail.com>
2019-11-26 11:47:21 -0800

    Create success component

--- basics-assignment-1-start/src/app/app.module.ts
+++ basics-assignment-1-start/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningComponent
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
