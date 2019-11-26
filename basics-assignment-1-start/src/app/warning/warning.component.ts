9844c9cdec39f1fac07c96a615876179502bede6
Jupiter <jupiterbaudot@gmail.com>
2019-11-26 11:15:11 -0800

    Add style literals in warning component

--- basics-assignment-1-start/src/app/warning/warning.component.ts
+++ basics-assignment-1-start/src/app/warning/warning.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'warning-alert',
     templateUrl: './warning.component.html',
-    stylesUrl: ['.warning.component.css']
+    styles: [`
+        p {
+            color: red;
+        }
+    `]
 })
 
 export class WarningComponent {
