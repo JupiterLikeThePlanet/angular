b0f98e2fe5cd36f1c9dc22e918257f4419d79d8c
Jupiter <jupiterbaudot@gmail.com>
2019-11-25 17:08:48 -0800

    Flesh our warning.component.ts

---
+++ basics-assignment-1-start/src/app/warning/warning.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'warning-alert',
+    templateUrl: './warning.component.html',
+    stylesUrl: ['.warning.component.css']
+}); 
