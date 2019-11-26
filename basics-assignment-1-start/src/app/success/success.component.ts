96504358340419e2ba855415d4027e513420db28
Jupiter <jupiterbaudot@gmail.com>
2019-11-26 13:10:49 -0800

    Flesh out success styles

--- basics-assignment-1-start/src/app/success/success.component.ts
+++ basics-assignment-1-start/src/app/success/success.component.ts
@@ -3,7 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'success-alert',
   templateUrl: './success.component.html',
-  styleUrls: ['./success.component.css']
+  // styleUrls: ['./success.component.css']
+  styles: [`
+        p {
+          color: white;
+        }
+
+        .success-bg {
+          background-color:green;
+          border-radius: 0.5em;
+          /* opacity: 12%; */
+        }
+  `]
 })
 export class SuccessComponent implements OnInit {
 
