6f06803d81a39eb02159484880b245cd5015f898
Jupiter <jupiterbaudot@gmail.com>
2019-11-25 17:34:17 -0800

    Fix superfluous semicolon and add warningcomponent successfully

--- basics-assignment-1-start/src/app/warning/warning.component.ts
+++ basics-assignment-1-start/src/app/warning/warning.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'warning-alert',
     templateUrl: './warning.component.html',
     stylesUrl: ['.warning.component.css']
-}); 
+})
+
+export class WarningComponent {
+
+}
