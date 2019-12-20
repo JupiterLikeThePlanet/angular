997424014590d327090864aabf3cd21ebd7df02e
Jupiter <jupiterbaudot@gmail.com>
2019-12-19 19:24:47 -0600

    Create databinding component and add input

--- basics-assignment-2-start/src/app/app.module.ts
+++ basics-assignment-2-start/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DataBindingComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+
+export class AppModule {
+
+}
