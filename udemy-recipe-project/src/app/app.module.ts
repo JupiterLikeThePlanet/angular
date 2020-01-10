dfab9c3b9f3738313c46af6fa2e818b13b3d8adf
Jupiter <jupiterbaudot@gmail.com>
2020-01-10 10:22:02 -0800

    Create recipe component files

--- udemy-recipe-project/src/app/app.module.ts
+++ udemy-recipe-project/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeComponent } from './recipes/recipe/recipe.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 
 
@@ -14,7 +18,11 @@ import { HeaderComponent } from './header/header.component';
     AppComponent,
     ShoppingListComponent,
     RecipesComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
